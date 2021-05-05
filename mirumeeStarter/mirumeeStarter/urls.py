ef61db41aa24397b1f25c7108ca66d5f4cb190c0
Pelet0n <hubert.zdk05@gmail.com>
2021-05-05 19:04:19 +0200

    'Add-product'

--- mirumeeStarter/mirumeeStarter/urls.py
+++ mirumeeStarter/mirumeeStarter/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('polls/',include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('graphql/', GraphQLView.as_view(graphiql=True)),
 ]
 
